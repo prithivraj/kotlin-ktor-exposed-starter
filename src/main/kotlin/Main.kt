0d1ff2cc3381a36f983d516b8ad9e04ed26fdc0f
Prithiv Raj <prithivraj007@gmail.com>
2018-10-25 14:52:37 +0530

    Status pages added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.Application
+import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
+import io.ktor.features.StatusPages
 import io.ktor.jackson.jackson
+import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -17,6 +20,12 @@ fun Application.module() {
     install(DefaultHeaders)
     install(CallLogging)
     install(WebSockets)
+
+    install(StatusPages){
+        exception<Throwable> { cause ->
+            call.respond(cause.message.toString())
+        }
+    }
 
     install(ContentNegotiation) {
         jackson {
@@ -35,5 +44,10 @@ fun Application.module() {
 }
 
 fun main(args: Array<String>) {
-    embeddedServer(Netty, 8080, watchPaths = listOf("MainKt"), module = Application::module).start()
+    embeddedServer(
+            Netty,
+            8080,
+            watchPaths = listOf("MainKt"),
+            module = Application::module
+    ).start()
 }
